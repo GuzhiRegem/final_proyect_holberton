e1835a69231ac970c66f9b50cd5390c079b5750a
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-03-19 23:18:17 -0300

    commit

--- node_modules/maplibre-gl/src/index.test.ts
+++
@@ -1,7 +0,0 @@
-import maplibre from './index';
-
-describe('maplibre', () => {
-    test('workerCount', () => {
-        expect(typeof maplibre.workerCount === 'number').toBeTruthy();
-    });
-});
